a9249d2df1c8719052fd7b93e959fdb8170209e7
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2014-12-23 18:13:50 +0900

    add root

--- client_app/config/routes.rb
+++ client_app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'example/credentialed_request'
+  root 'example#credentialed_request'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
